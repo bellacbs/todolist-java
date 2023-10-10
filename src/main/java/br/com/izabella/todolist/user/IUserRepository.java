a696702d2a1e73d00f1524abe6b7f35ce51f9539
Izabella <bellacbrandao@gmail.com>
2023-10-10 09:42:21 -0300

    jpa, h2-console,, criacao de usuario

---
+++ src/main/java/br/com/izabella/todolist/user/IUserRepository.java
@@ -0,0 +1,8 @@
+package br.com.izabella.todolist.user;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
--- src/main/java/br/com/izabella/todolist/user/UserController.java
+++ src/main/java/br/com/izabella/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.izabella.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel);
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("usuario ja existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @GetMapping("")
+    public String getUser() {
+        return "aqui voce pega o user";
     }
 }
