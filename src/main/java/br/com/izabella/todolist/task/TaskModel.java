ec61225892f4e4c8db33b35e0b5347f1cdd7ed55
Izabella <bellacbrandao@gmail.com>
2023-10-13 18:41:05 -0300

    endpoint de criar tarefa

--- src/main/java/br/com/izabella/todolist/task/TaskModel.java
+++ src/main/java/br/com/izabella/todolist/task/TaskModel.java
@@ -31,8 +31,8 @@ public class TaskModel {
     private LocalDateTime createAt;
 
     public void setTitle(String title) throws Exception {
-        if(title.length() > 50) {
-            throw new Exception("O titulo deve conter no maximo 50 caracteres")
+        if (title.length() > 50) {
+            throw new Exception("O titulo deve conter no maximo 50 caracteres");
         }
         this.title = title;
     }
