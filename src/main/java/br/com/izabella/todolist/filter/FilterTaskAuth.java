f1fe42b19072d96a3eea45ae288e887239230645
Izabella <bellacbrandao@gmail.com>
2023-10-13 18:09:24 -0300

    errors exception

---
+++ src/main/java/br/com/izabella/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,20 @@
+package br.com.izabella.todolist.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class FilterTaskAuth implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doFilter'");
+    }
+
+}
--- src/main/java/br/com/izabella/todolist/user/UserController.java
+++ src/main/java/br/com/izabella/todolist/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,12 +24,10 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("usuario ja existe");
         }
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
         var userCreated = this.userRepository.save(userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
-    }
 
-    @GetMapping("")
-    public String getUser() {
-        return "aqui voce pega o user";
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
